676f61f391e948065841d391f4591b7d7aae9bf4
mevouc <meven.courouble@vouc.me>
2016-05-15 00:52:06 +0000

    Small changes before starting project

--- Makefile
+++ Makefile
@@ -2,13 +2,14 @@ CC=mcs
 PRJ=Quads
 SRC=$(PRJ).cs
 OUT=$(PRJ).exe
+REF=System.Drawing.dll
 
 all:$(OUT)
 	
 $(PRJ):$(OUT)
 
 $(OUT):
-	$(CC) $(SRC)
+	$(CC) -r:$(REF) $(SRC)
 
 clean:
 	rm -f $(OUT)
--- Quads.cs
+++ Quads.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Drawing;
 
 public class Quads
 {
-  static public void Main()
+  static private Bitmap img;
+  static private int N;
+
+  static private bool Run()
   {
-    Console.WriteLine("Test.");
+    return true;
+  }
+
+  static public int Main(string[] args)
+  {
+    if (args.Length != 2)
+    {
+      Console.Error.WriteLine("This program takes 2 arguments.");
+      return 1;
+    }
+    img = new Bitmap(args[0]);
+    if (!Int32.TryParse(args[1], out N))
+    {
+      Console.Error.WriteLine("The last argument should be an integer.");
+      return 1;
+    }
+    return Run() ? 0 : 1;
   }
 }
